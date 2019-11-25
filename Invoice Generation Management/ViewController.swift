6ee2897d1aecf2c0113f8d3535230236f70499bd
Harshit Jindal <harshit.jindal@icloud.com>
2019-11-25 11:38:22 +0530

    Initial Commit

---
+++ Invoice Generation Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Invoice Generation Management
+//
+//  Created by HARSHIT JINDAL on 25/11/19.
+//  Copyright © 2019 HARSHIT JINDAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
