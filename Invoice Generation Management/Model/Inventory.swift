cca73cccb8ef26a75dd58638cb354fea2c589e2f
Harshit Jindal <harshit.jindal@icloud.com>
2019-11-26 22:03:22 +0530

    Add Inventory Model

---
+++ Invoice Generation Management/Model/Inventory.swift
@@ -0,0 +1,15 @@
+//
+//  Inventory.swift
+//  Invoice Generation Management
+//
+//  Created by HARSHIT JINDAL on 26/11/19.
+//  Copyright © 2019 HARSHIT JINDAL. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Inventory: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var qty: Int = 0
+}
---
+++ Invoice Generation Management/View/InventoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InventoryTableViewCell.swift
+//  Invoice Generation Management
+//
+//  Created by HARSHIT JINDAL on 26/11/19.
+//  Copyright © 2019 HARSHIT JINDAL. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
